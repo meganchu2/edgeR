52c111156355278a4c7c3ad127e7f2204a0e064f
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-17 10:00:16 +0000

    gini() now supports NA values and avoids integer overflow.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/edgeR@128759 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gini.R
+++ R/gini.R
@@ -1,10 +1,15 @@
 gini <- function(x)
 #	Gini diversity index for columns of a numeric matrix
 #	Gordon Smyth
-#	5 Feb 2016
+#	Created 5 Feb 2016. Last revised 17 April 2017.
 {
+	x <- as.matrix(x)
 	d <- dim(x)
-	if(is.null(d)) d <- dim(x) <- c(length(x),1)
 	for (j in 1:d[2]) x[,j] <- sort.int(x[,j],na.last=TRUE)
-	(2*colSums((1:d[1])*x)/colSums(x)-d[1]-1)/d[1]
+	i <- 1:d[1]
+	m <- 0.75*d[1]
+	S1 <- colSums((i-m)*x , na.rm=TRUE)
+	S2 <- colSums(x, na.rm=TRUE)
+	(2*(S1/S2+m)-d[1]-1)/d[1]
 }
+
